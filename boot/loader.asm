95c552c49ffa852672b0bcd3c25a19884f7f6945
softwaresale <chucks.8090@gmail.com>
2017-05-17 18:27:28 -0400

    I got ChaosOS workign again

--- boot/loader.asm
+++ boot/loader.asm
@@ -1,7 +1,5 @@
 global loader					; the entry point for elf
 
-section .multiboot
-align 4
 MAGIC_NUMBER 	  equ 0x1BADB002		; the magic number
 FLAGS	     	  equ 0x0 			; multiboot flags
 CHECKSUM          equ -MAGIC_NUMBER		; calculate the checksum
@@ -14,7 +12,7 @@ align 4
 kernel_stack:
 	resb KERNEL_STACK_SIZE
 
-section .text 					; start of code section
+section .multiboot 				; start of code section
 align 4						; align 4 bytes
 	dd MAGIC_NUMBER				; write the magic number to machine code
 	dd FLAGS
